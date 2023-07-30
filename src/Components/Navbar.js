72c075749d602345caf4080e3d097f2e3504835c
GeekWolf007 <geeksingal@gmail.com>
2023-07-30 13:58:34 +0530

    style: navbar positioning

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,14 +3,22 @@ import * as styles from '../Styles/Navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav className={styles["navbar"]}>
+        <React.Fragment>
+        <div className={styles['navbarContainer']}>
+        <nav className={styles['navbarLeft']}>
             <ul>
                 <li className={styles['navLinks']}>ABOUT US</li>
                 <li className={styles['navLinks']}>EVENTS</li>
+            </ul>
+        </nav>
+        <nav className={styles["navbarRight"]}>
+            <ul>
                 <li className={styles['navLinks']}>CONTACT US</li>
                 <li className={styles['navLinks']}>SPONSORS</li>
             </ul>
         </nav>
+        </div>
+        </React.Fragment>
     );
 };
 
