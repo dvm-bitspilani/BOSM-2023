3dad4e99fa38786e1994390f623f2daa79ed39fa
GeekWolf007 <geeksingal@gmail.com>
2023-08-02 22:33:47 +0530

    half form

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Layout from '../Components/Layout';
-import rp_bg from "../images/rp_bg.png";
-import rp_bg_icons from "../images/rp_bg_icons.png";
+import cross from "../images/cross.svg";
+import logo from "../images/logo.svg"
 import * as styles from "../Styles/Content.module.css";
+import Multiselect from 'multiselect-react-dropdown';
+import { navigate } from 'gatsby';
 
 const registration = () => {
 
+    const goBack = () => {
+        navigate(-1);
+    };
 
   return (
     <Layout
@@ -14,7 +19,36 @@ const registration = () => {
         content={
             <section className={styles["regPage"]}>
                 <div className={styles["rpIcons"]}></div>
+                <div className={styles["rpHeadingContainer"]}>
+                    <img src={logo} />
+                    <h3>REGISTRATION</h3>
+                    <img src={cross} onClick={goBack} className={styles["cross"]} />
+                </div>
+                <form className={styles["regForm"]}> 
+                    <div className={styles["formSingleInput"]}>
+                        <label htmlFor='name'>NAME</label>
+                        <input className={styles["regInput"]} id='name' />
 
+                        <label htmlFor='email'>E-MAIL</label>
+                        <input className={styles["regInput"]} id='email' />
+
+                        <label htmlFor='phone'>PHONE</label>
+                        <input className={styles["regInput"]} id='phone' />
+
+                        <label htmlFor='gender'>GENDER</label>
+                        <div className={styles["radioBtns"]}>
+                            <input id='genderM' type='radio' name='gender' value="Male" />
+                            <label htmlFor='genderM'>Male</label>
+                            <input id='genderF' type='radio' name='gender' value="Female" />
+                            <label htmlFor='genderF'>Female</label>
+                            <input id='genderO' type='radio' name='gender' value="Others" />
+                            <label htmlFor='genderO'>Others</label>
+                        </div>
+                    </div>
+                    <div className={styles["formMultiInput"]}>
+
+                    </div>
+                </form>
             </section>
         }
     />
