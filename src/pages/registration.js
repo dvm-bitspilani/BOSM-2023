f47646a995b793ce6e94f050d6ecc5110b651c00
GeekWolf007 <geeksingal@gmail.com>
2023-08-03 01:28:32 +0530

    feat: implement react single select

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -3,14 +3,80 @@ import Layout from '../Components/Layout';
 import cross from "../images/cross.svg";
 import logo from "../images/logo.svg"
 import * as styles from "../Styles/Content.module.css";
-import Multiselect from 'multiselect-react-dropdown';
 import { navigate } from 'gatsby';
+import Select from 'react-select';
 
 const registration = () => {
 
     const goBack = () => {
         navigate(-1);
     };
+
+    const customStyles = {
+        control: (provided, state) => ({
+          ...provided,
+          height: '3rem',
+          backgroundColor: 'transparent',
+          border: state.isFocused ? '2px solid white' : '2px solid white',
+          '&:hover': {
+            borderColor: 'white',
+          },
+          cursor: 'text',
+        }),
+        singleValue: (provided) => ({
+          ...provided,
+          color: '#eee',
+          fontFamily: 'Montserrat',
+          fontSize: '1.8rem',
+          fontWeight: 700,
+          paddingLeft: '1rem',
+        }),
+        option: (provided, state) => ({
+          ...provided,
+          color: state.isSelected ? '#ffd700' : '#111',
+          backgroundColor: state.isSelected ? 'transparent' : 'transparent',
+          fontFamily: 'Montserrat',
+          fontSize: '1.6rem',
+          fontWeight: 600,
+          paddingLeft: '1rem',
+        }),
+        dropdownIndicator: (provided, state) => ({
+          ...provided,
+          color: 'white',
+        }),
+        placeholder: () => ({ display: 'none' }),
+        container: (provided) => ({
+          ...provided,
+          overflow: 'visible',
+        }),
+        input: (provided) => ({
+          ...provided,
+          color: '#eee',
+          fontFamily: 'Montserrat',
+          fontSize: '1.8rem',
+          fontWeight: 700,
+          paddingLeft: '1rem',
+        }),
+      };
+      
+           
+           
+    const collegeOptions = [
+        {value:"IIT Bombay" , label: "IIT Bombay"},
+        {value:"IIT Delhi" , label: "IIT Delhi"},
+        {value:"IIT Madras" , label: "IIT Madras"},
+        {value:"BITS Pilani" , label: "BITS Pilani"},
+    ];
+    const cityOptions = [
+        {value:"Bombay" , label: "Bombay"},
+        {value:"Delhi" , label: "Delhi"},
+        {value:"Madras" , label: "Madras"},
+        {value:"Pilani" , label: "Pilani"},
+    ];
+
+    const handleChange = selectedCollege =>{
+        console.log(selectedCollege);
+    }
 
   return (
     <Layout
@@ -46,7 +112,11 @@ const registration = () => {
                         </div>
                     </div>
                     <div className={styles["formMultiInput"]}>
+                        <label htmlFor='college'>COLLEGE</label>
+                        <Select options={collegeOptions} onChange={handleChange} styles={customStyles}  />
 
+                        <label htmlFor='city'>CITY</label>
+                        <Select options={cityOptions} onChange={handleChange} styles={customStyles}  />
                     </div>
                 </form>
             </section>
