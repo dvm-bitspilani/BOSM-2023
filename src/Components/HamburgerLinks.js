4817ba1241c746c17735681468c2310d8cdad114
yoshicon <shreyaskgantayet908@gmail.com>
2023-08-08 03:19:59 +0530

    Desktop Hamburger done

--- src/Components/HamburgerLinks.js
+++ src/Components/HamburgerLinks.js
@@ -3,20 +3,42 @@ import * as styles from "../Styles/Hamburger.module.css";
 import { motion } from "framer-motion";
 
 export default function HamburgerLinks({ img, text, color }) {
+    const [isHovered, setIsHovered] = React.useState(false);
+
+    const variants = {
+        hovered: {
+            top: 0,
+            bottom: 0,
+            transition: {
+                delay: 0,
+                duration: 0.3,
+                ease: "easeInOut",
+            },
+        },
+        notHovered: {
+            top: "70%",
+            bottom: "10%",
+            transition: {
+                delay: 0,
+                duration: 0.3,
+                ease: "easeInOut",
+            },
+        },
+    };
+
   return (
     <div
       style={{ backgroundImage: `url(${img})` }}
       className={styles["container"]}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
     >
       <img src={img} alt={text} />
-      <motion.div style={{ backgroundColor: `${color}` }} className={styles["text"]}
-      whileHover={{
-        scale: 1.1,
-        transition: {
-            duration: 0.5,
-            ease: "easeInOut",
-        },
-      }}
+      <motion.div
+        style={{ backgroundColor: `${color}` }}
+        className={styles["text"]}
+        variants={variants}
+        animate={isHovered ? "hovered" : "notHovered"}
       >
         {text}
       </motion.div>
