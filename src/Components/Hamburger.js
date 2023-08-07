c3a22e19e908f411cc1fbf49c69630da4cc4eb1e
yoshicon <shreyaskgantayet908@gmail.com>
2023-08-08 01:40:25 +0530

    Disabled Scrolling on hamburger open

--- src/Components/Hamburger.js
+++ src/Components/Hamburger.js
@@ -9,18 +9,21 @@ export default function Hamburger({closeHam, isHamOpen}) {
             setTimeout(() => {
                 hamburger.style.zIndex = 100;
             }, 2000);
+            document.documentElement.style.overflow = "hidden";
         }else{
             hamburger.style.zIndex = -3;
+            document.documentElement.style.overflow = "scroll";
         }
         
     }, [isHamOpen]);
+
   return (
     <div className={styles["hamburger"]}>
       {/* <button onClick={closeHam}>
         Close
       </button> */}
       {/* <div>Hamburger</div> */}
-      
+
     </div>
   );
 }
