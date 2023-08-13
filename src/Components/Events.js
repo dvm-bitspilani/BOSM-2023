56cc650dc74f14562f785bf2fd5448141411ac42
GeekWolf007 <geeksingal@gmail.com>
2023-08-13 22:05:02 +0530

    refactor

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,11 +1,17 @@
 import React from "react";
 import * as styles from "../Styles/Events.module.css";
+import Tennis from "../images/tennis.png";
 
 const Events = props =>{
 
     return(
         <section className={styles.eventsSection}>
-
+            <h3>EVENTS</h3>
+            <div className={styles.eventsContainer}>
+                <div className={styles.leftSport}><img src={Tennis} /></div>
+                <div><img src={Tennis} /></div>
+                <div className={styles.rightSport}><img src={Tennis} /></div>
+            </div>
         </section>
     );
 };
