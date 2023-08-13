5a10ad60d9a0e28dc795e638e9c709dd5a7fe8ad
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-08-13 16:57:51 +0530

    back button in gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -4,6 +4,9 @@ import Layout from "../Components/Layout";
 import Cursor from "../images/cursor.png";
 import "../Styles/globals.css"
 import { useState, useEffect } from "react";
+import arrow2 from "../images/arrow.svg";
+import { navigate } from "gatsby";
+
 
 const Gallery = props => {
 
@@ -13,37 +16,41 @@ const Gallery = props => {
         const cursor = document.getElementById("cursor");
         const cursorCircle = document.getElementById("cursorFollower");
         const cursorImg = document.getElementById("cursorImg");
-    
+
         if (window.innerWidth > 1000) {
-          window.onpointermove = (event) => {
-            const { clientX, clientY } = event;
-    
-            cursor.style.display = "block";
-            cursorCircle.style.display = "block";
-            cursor.style.opacity = "1";
-            cursorCircle.style.opacity = "1";
-            cursor.style.left = `${clientX}px`;
-            cursor.style.top = `${clientY}px`;
-    
-            let _clientX = clientX - 52;
-            let _clientY = clientY - 52;
-    
-            cursorCircle.animate(
-              {
-                left: `${_clientX}px`,
-                top: `${_clientY}px`,
-              },
-              { duration: 500, fill: "forwards" }
-            );
-          };
+            window.onpointermove = (event) => {
+                const { clientX, clientY } = event;
+
+                cursor.style.display = "block";
+                cursorCircle.style.display = "block";
+                cursor.style.opacity = "1";
+                cursorCircle.style.opacity = "1";
+                cursor.style.left = `${clientX}px`;
+                cursor.style.top = `${clientY}px`;
+
+                let _clientX = clientX - 52;
+                let _clientY = clientY - 52;
+
+                cursorCircle.animate(
+                    {
+                        left: `${_clientX}px`,
+                        top: `${_clientY}px`,
+                    },
+                    { duration: 500, fill: "forwards" }
+                );
+            };
         }
-        setIsLoading(false); 
+        setIsLoading(false);
         return () => {
-          window.onpointermove = null;
-          window.scrollTo(0, 0);
+            window.onpointermove = null;
+            window.scrollTo(0, 0);
         };
-      }, [isLoading]);
-    
+    }, [isLoading]);
+
+    const closeButtonHandler = () => {
+        navigate("/");
+    };
+
 
     return (
         <>  <div className="cursor" id="cursor"></div>
@@ -53,23 +60,27 @@ const Gallery = props => {
             <main
                 className={styles["gallery"]}
                 isHamOpen={false}>
+
                 <div className={styles["wrapper"]}>
-                <div className={styles["gridContainer"]}>
-                    <div className={styles["box1"]}>Gallery</div>
-                    <div className={styles["box2"]}></div>
-                    <div className={styles["box3"]}></div>
-                    <div className={styles["box4"]}></div>
-                    <div className={styles["box5"]}></div>
-                    <div className={styles["box6"]}>
-                        <p className={styles["aboutHeading"]}>about</p>
-                        <p className={styles["aboutText"]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie posuere eros, vitae bibendum lorem. Quisque volutpat eget lacus sit amet commodo. Fusce condimentum, neque vel accumsan finibus, felis nisi suscipit </p>
+                    <img
+                        src={arrow2} onClick={closeButtonHandler}
+                    />
+                    <div className={styles["gridContainer"]}>
+                        <div className={styles["box1"]}>Gallery</div>
+                        <div className={styles["box2"]}></div>
+                        <div className={styles["box3"]}></div>
+                        <div className={styles["box4"]}></div>
+                        <div className={styles["box5"]}></div>
+                        <div className={styles["box6"]}>
+                            <p className={styles["aboutHeading"]}>about</p>
+                            <p className={styles["aboutText"]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie posuere eros, vitae bibendum lorem. Quisque volutpat eget lacus sit amet commodo. Fusce condimentum, neque vel accumsan finibus, felis nisi suscipit </p>
+                        </div>
+                        <div className={styles["box7"]}></div>
+                        <div className={styles["box8"]}></div>
+                        <div className={styles["box9"]}></div>
+                        <div className={styles["box10"]}></div>
                     </div>
-                    <div className={styles["box7"]}></div>
-                    <div className={styles["box8"]}></div>
-                    <div className={styles["box9"]}></div>
-                    <div className={styles["box10"]}></div>
-                </div>
-                <div className={styles["tagline"]}>roar of resilience</div>
+                    <div className={styles["tagline"]}>roar of resilience</div>
                 </div>
             </main>
         </>
