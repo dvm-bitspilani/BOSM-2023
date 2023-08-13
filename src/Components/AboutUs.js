a6cb32a795f419b855bf6bdbc4ed6c42d4c08924
Luv Gupta <lg5122003@gmail.com>
2023-08-13 21:13:07 +0530

    feat: add About Us content

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,26 +1,54 @@
-import React, { useRef } from "react";
-import { motion,  useScroll } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import { motion, useScroll } from "framer-motion";
 import * as about from "../Styles/About.module.css";
 
 
-const AboutUs =(props) =>{
+const AboutUs = (props) => {
 
-    const ref = useRef(null)
-    const { scrollYProgress } = useScroll({
-        target: ref,
-    });
+    const { scrollYProgress } = useScroll();
+    // useEffect(()=>{
+    //     const page = document.querySelector(`.${about["aboutPage"]}`)
+    //     page.style.transform = `translateY(${scrollYProgress}px)`;
+    // },[])
 
-    return(
-        <main ref={ref} className={about["aboutPage"]}>
-        <div className={about["topContainer"]}>
-            <div className={about["heading"]}>
-                <h1>ABOUT US</h1>
+    return (
+        <motion.main
+            className={about["aboutPage"]}
+            style={{ borderRadius: scrollYProgress }}
+        >
+            <div className={about["topContainer"]}>
+                <div className={about["heading"]}>
+                    <h1>ABOUT US</h1>
+                </div>
             </div>
-        </div>
-        <div className={about["bottomContainer"]}>
+            <div className={about["bottomContainer"]}>
+                <div className={about["contentContainer"]}>
+                    <div className={about["group"]}>
+                        <div className={about["title"]}>
+                            <h1>BOSM</h1>
+                        </div>
+                        <div className={about["desc"]}>
+                            One of the biggest sports extravaganzas in the Country, BITS Open Sports Meet (BOSM), is back with its 36th edition this winter. The annual sports fest of BITS Pilani, Pilani Campus, since its inception in 1986, has been a stage for college teams to showcase their supremacy by competing amongst the nation's best talents.
+                        </div>
+                    </div>
+                    <div className={about["group"]}>
+                        <div className={about["title"]}>
+                            <h1>THEME</h1>
+                        </div>
+                        <div className={about["desc"]}>
+                            The theme for BOSM 2022 is "Roar of Resilience." It signifies the stage being set for the sporting event and glory tells us about the success stories and efforts that one puts into achieving their feats.
+                        </div></div>
+                    <div className={about["group"]}>
+                        <div className={about["title"]}>
+                            <h1>SIGNIFICANCE</h1>
+                        </div>
+                        <div className={about["desc"]}>
+                            This theme encourages participants to face challenges with courage, bounce back from setbacks, and demonstrate their unwavering determination, fostering personal growth, perseverance, and the celebration of triumphs against all odds.                </div>
+                    </div>
+                </div>
+            </div>
 
-        </div>
-        </main>
+        </motion.main>
     );
 };
 
