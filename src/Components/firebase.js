ec40814bcebb60c9140e6469625f489715ffebd8
GeekWolf007 <geeksingal@gmail.com>
2023-08-14 19:25:40 +0530

    photo upload

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,4 +1,6 @@
 // Import the functions you need from the SDKs you need
+
+  
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getStorage} from "firebase/storage"
