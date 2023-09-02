62614cc716a583633c6542706c0ea9beb5c5f058
Luv Gupta <lg5122003@gmail.com>
2023-09-02 23:24:45 +0530

    style: add hover animation on contacts images

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -10,28 +10,27 @@ export default function Profile(props) {
 
     return (
         <div className={contact["profileContainer"]}>
-            <div className={contact["profileImg"]}
-                style={{
-                    backgroundImage: `url(${props.img})`, backgroundColor: "#5E7F88"
-                }}
-            ></div>
+            <div className={contact["profileImg"]} style={{ backgroundColor: "#5E7F88" }}>
+                <div className={contact["profileAsset"]} style={{ backgroundImage: `url(${props.img})` }}>
+                </div>
+            </div>
             <div className={contact["profileName"]}>
                 <div>{props.name}</div>
             </div>
             <div className={contact["profileDept"]}>
                 <div>{props.dept}</div>
             </div>
-            <div className={contact["icons"]} style={{display: props.cossacSwitchBtn ? 'flex':'none'}}>
+            <div className={contact["icons"]} style={{ display: props.cossacSwitchBtn ? 'flex' : 'none' }}>
                 <motion.div whileHover={{ scale: 1.2 }}
                     whileTap={{ scale: 1 }}>
                     <a href={`tel:${props.phone}`}>
-                    <img src={PhoneIcon} alt="phone" />
-                </a></motion.div>
-            <motion.div whileHover={{ scale: 1.2 }}
-                whileTap={{ scale: 1 }}>
-                <a href={`mailto:${props.email}`}>
-                <img src={MailIcon} alt="mail" /></a></motion.div>
-        </div>
+                        <img src={PhoneIcon} alt="phone" />
+                    </a></motion.div>
+                <motion.div whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 1 }}>
+                    <a href={`mailto:${props.email}`}>
+                        <img src={MailIcon} alt="mail" /></a></motion.div>
+            </div>
         </div >
     )
 }
