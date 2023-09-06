d14cb2496669031053607e0c5b2965be328d8943
yoshicon <shreyaskgantayet908@gmail.com>
2023-09-07 03:06:57 +0530

    Added scoreboard

---
+++ src/Components/ScoreCard.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import * as styles from "../styles/Scoreboard.module.css";
+
+export default function ScoreCard({ data = null }) {
+  const [team1, setTeam1] = React.useState(null);
+  const [team2, setTeam2] = React.useState(null);
+  const [score1, setScore1] = React.useState(null);
+  const [score2, setScore2] = React.useState(null);
+  const [event, setEvent] = React.useState(null);
+  const [venue, setVenue] = React.useState(null);
+
+  React.useEffect(() => {
+    if (data && data.team_scores.length === 2) {
+      setTeam1(data.team_scores[0].team);
+      setTeam2(data.team_scores[1].team);
+      setScore1(data.team_scores[0].score);
+      setScore2(data.team_scores[1].score);
+    } else if (data && data.team_scores.length > 2) {
+      setTeam1(data.team_scores[0].team);
+      setScore1(data.team_scores[0].score);
+    }
+    if (data) {
+      setEvent(data.sport + " " + data.round_name);
+      setVenue(data.venue);
+      console.log(data.team_scores[1]);
+    }
+
+  }, [data]);
+
+  return (
+    <div className={styles.scoreCard}>
+      <div className={styles.teamNames}>
+        <span className={`${styles.teamName} ${styles.winningTeam}`}>
+          {data === null ? "BITS P" : team1}
+        </span>
+        {data !== null && data.team_scores.length === 2 && (
+          <>
+            <span className={styles.vsText}>VS</span>
+            <span className={`${styles.teamName}`}>{team2}</span>
+          </>
+        )}
+      </div>
+      <div className={styles.score}>
+        <span className={`${styles.teamScore}`}>{score1}</span>
+        {data !== null && data.team_scores.length === 2 && (
+          <>
+            <span> - </span>
+            <span className={`${styles.teamScore}`}>{score2}</span>
+          </>
+        )}
+      </div>
+      <div className={styles.event}>
+        {data === null ? (
+          <span className={styles.eventName}>Football Round 2</span>
+        ) : (
+          <span className={styles.eventName}>{event}</span>
+        )}
+      </div>
+      <div className={styles.venue}>
+        <span className={styles.venueName}>
+          Venue : {data === null ? "GymG" : venue}
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/scoreboard/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import ScoreCard from "../../Components/ScoreCard";
+// import Layout from '../../Components/Layout';
+import * as styles from "../../styles/Scoreboard.module.css";
+import { navigate } from "gatsby";
+
+import logoImg from "../../images/logo.svg";
+
+export default function Index() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    const socket = new WebSocket("wss://test.bitsbosm.org/2023/ws/live_score/");
+    socket.onopen = (e) => {
+      console.log("connected");
+      //   console.log(e.data)
+      fetch("https://test.bitsbosm.org/2023/live-score/send-data")
+        .then((res) => res.json())
+        .then((data) => {
+          setData(JSON.parse(data));
+        });
+    };
+    socket.onmessage = (event) => {
+      try {
+        const data = JSON.parse(event.data);
+        setData(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    socket.onclose = () => {
+      console.log("disconnected");
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
+  return (
+    <main className={styles.pageContainer}>
+      <div className={styles.header}>
+        <div className={styles.logoDiv} onClick={() => navigate("/")}>
+          <img src={logoImg} alt="logo" />
+        </div>
+        <h1>SCOREBOARD</h1>
+      </div>
+      <div className={styles.scoreboardGrid}>
+        {data
+          ? data.map((item, index) => {
+              if (item.team_scores.length < 2) {
+                return null;
+              } else {
+                return <ScoreCard key={index} data={item} />;
+              }
+            })
+          : null}
+      </div>
+    </main>
+  );
+}
