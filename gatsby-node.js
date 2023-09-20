130ff037457924606114d7c5da6a9f3c535d2e51
yoshicon <shreyaskgantayet908@gmail.com>
2023-09-21 02:33:26 +0530

    test

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,11 @@
+// import path from "node:path";
+// import CopyWebpackPlugin from "copy-webpack-plugin";
+
+// const cMapsDir = path.join(
+//   path.dirname(require.resolve("pdfjs-dist/package.json")),
+//   "cmaps"
+// );
+
 exports.onCreateWebpackConfig = ({
   stage,
   rules,
@@ -6,15 +14,32 @@ exports.onCreateWebpackConfig = ({
   actions,
 }) => {
   actions.setWebpackConfig({
-    module: {
-      rules: [
-        // Other rules...
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: "node-loader",
-        },
-      ],
-    },
+    // plugins: [
+    //   new CopyWebpackPlugin({
+    //     patterns: [
+    //       {
+    //         from: cMapsDir,
+    //         to: "cmaps/",
+    //       },
+    //     ],
+    //   }),
+    // ],
+
+    // module: {
+    //   rules: [
+    //     // Other rules...
+    //     {
+    //       test: /\.(js|jsx)$/,
+    //       exclude: /node_modules/,
+    //       // use: 'node-loader',
+    //     },
+    //   ],
+    // },
+
+    externals: [
+      {
+        canvas: "canvas",
+      },
+    ],
   });
 };
